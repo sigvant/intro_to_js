db0f1f3293439ad8514236ddaf630f3e8fb2d365
sigvant <bnsviv@gmail.com>
2021-02-08 14:52:51 -0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+var nome = "Rafael Galleani";
+var n1 = 29;
+var n2 = 10;
+var frase = "Japão é o melhor time do mundo."
+
+// alert(nome + " tem " + idade + " anos.");
+// alert(idade + idade2)
+
+console.log(nome);
+console.log(n1 + n2);
+console.log(frase.replace("Japão" , "Brasil"));
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
+
