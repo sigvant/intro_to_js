5bcdd1a6fd3295501be80fd88a351a513073cc67
sigvant <bnsviv@gmail.com>
2021-02-20 10:09:08 -0300

    final commit

--- js/main.js
+++ js/main.js
@@ -1,15 +1,77 @@
 
-var nome = "Rafael Galleani";
-var n1 = 29;
-var n2 = 10;
-var frase = "Japão é o melhor time do mundo."
+function clicou(){
+	document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+	//console.log(document.getElementById("agradecimento"));
+}
 
-// alert(nome + " tem " + idade + " anos.");
-// alert(idade + idade2)
+function redirecionar(){
+	window.open("https://www.google.com")
+}
 
-console.log(nome);
-console.log(n1 + n2);
-console.log(frase.replace("Japão" , "Brasil"));
-console.log(frase.toUpperCase());
-console.log(frase.toLowerCase());
+function trocar(element){
+	element.innerHTML = "Obrigado por passar o mouse"
+	// alert("trocar texto");
+}
+
+function voltar(element){
+	element.innerHTML = "Passe o mouse aqui"
+}
+
+function load() {
+	alert("page was loaded succesfully")
+}
+
+function funcaoChange (element){
+	console.log(element.value)
+}
+
+// var d = new Date();
+// alert(d.getMinutes());
+
+// var count;
+// for(count=1; count < 5; count++){
+// 	alert(count);
+// }
+
+
+// var count = 0;
+// while(count <= 5){
+// 	console.log(count);
+// 	count = count + 1;
+// }
+
+// var nome = "Rafael Galleani";
+// var n1 = 29;
+// var n2 = 10;
+// var frase = "Japão é o melhor time do mundo."
+
+// // alert(nome + " tem " + idade + " anos.");
+// // alert(idade + idade2)
+
+// console.log(nome);
+// console.log(n1 + n2);
+// console.log(frase.replace("Japão" , "Brasil"));
+// console.log(frase.toUpperCase());
+// console.log(frase.toLowerCase());
+
+// var lista = ['A', 'B', 'C'];
+// console.log(lista);
+// lista.push('D');
+// console.log(lista);
+// lista.push('F');
+// console.log(lista);
+// lista.pop();
+// console.log(lista);
+
+// console.log(lista.length);
+// console.log(lista.reverse());
+
+// console.log(lista.toString());
+// console.log(lista.join(" | "));
+
+// var frutas = [{nome: "Maçã", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+// console.log(frutas);
+// alert(frutas[1].nome);
+
+
 
